a4a4af02243138d593e7297a95e0a5e137744739
dinasour723 <162362750+dinasour723@users.noreply.github.com>
2024-03-11 08:04:27 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+import sys
+import os
+import tensorflow as tf
+from deskew_image import *
+
+PATH_DETECT_CKPT = 'path to detection model weight file'
+PATH_RECOG_CKPT = 'path to recognition model weight file'
+
+char_maps = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def plot_one_box(x, img, color=None, label=None, line_thickness=3):
+	# Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [0, 0, 0], thickness=tf, lineType=cv2.LINE_AA)
+    return img
+	
+def load_detect_graph():
+
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = detection_graph.as_graph_def()
+        with tf.io.gfile.GFile(PATH_DETECT_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+        sess = tf.compat.v1.Session(graph=detection_graph)
+
+    return detection_graph, sess
